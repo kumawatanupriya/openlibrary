1ed022f69e72298d425b4c8305b4cc0dba0dd16a
Prasanna Venkatesan <pvenkat4ever@gmail.com>
2012-12-05 14:58:33 +0530

    Removed barcode_images folder and added symlink creation in deploy.rb

--- create_barcode.sh
+++ create_barcode.sh
@@ -3,5 +3,5 @@ barcode -b \"$1\" -t 1x1+200+375 -o tmp/$1.ps
 echo "Converting postscript to png ..."
 convert -density 100 tmp/$1.ps -flatten tmp/$1.png
 echo "Cropping png ..."
-convert tmp/$1.png -crop 256x256+285+380 ../shared/barcode_images/$1.png
+convert tmp/$1.png -crop 256x256+285+380 barcode_images/$1.png
 rm tmp/$1.*
