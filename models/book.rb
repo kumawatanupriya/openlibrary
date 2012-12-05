f792cf4fd7612d2a0015ba5997db2e59bb87cf2d
Prasanna Venkatesan <pvenkat4ever@gmail.com>
2012-12-05 17:22:51 +0530

    Removed OpenLibrary check and save the book even if not found by Google API

--- models/book.rb
+++ models/book.rb
@@ -10,7 +10,7 @@ class Book
   has n, :reservation
 
   def self.create_from_openlibrary(isbn)
-    params = details_from_google(isbn) || details_from_openlibrary(isbn)
+    params = details_from_google(isbn) || {:isbn => isbn, :title => 'N/A', :author => 'N/A'}
     Book.new(params).tap do |b|
       b.book_copies << BookCopy.new
     end if params
@@ -19,17 +19,17 @@ class Book
   def self.details_from_openlibrary(isbn)
     details = ::Openlibrary::Data.find_by_isbn(isbn)
     {}.tap do |p|
-      p[:isbn]   = details.identifiers["isbn_13"] ? details.identifiers["isbn_13"][0] : isbn
-      p[:title]  = details.title
-      p[:author] = details.authors.map{|a| a["name"]}.join(", ") if details.authors
+      p[:isbn] = details.identifiers["isbn_13"] ? details.identifiers["isbn_13"][0] : isbn
+      p[:title] = details.title
+      p[:author] = details.authors.map { |a| a["name"] }.join(", ") if details.authors
       p[:photo_remote_url] = details.cover["medium"] if details.cover
     end if details
   end
 
   def self.details_from_google(isbn)
-    response = "https://www.googleapis.com/books/v1/volumes?q=isbn:#{isbn}&fields=items(volumeInfo(authors,description,imageLinks,industryIdentifiers,title))".to_uri.get.deserialise["items"][0]
-    return unless response
-    response = response["volumeInfo"]
+    items = "https://www.googleapis.com/books/v1/volumes?q=isbn:#{isbn}&fields=items(volumeInfo(authors,description,imageLinks,industryIdentifiers,title))".to_uri.get.deserialise["items"]
+    return nil if items.nil?
+    response = items[0]["volumeInfo"]
     {}.tap do |p|
       p[:isbn] = isbn
       p[:title] = response["title"]
