502997cb019820cd76c088a4933288319297d25b
Selva <k.n.selvakumar@gmail.com>
2012-12-05 15:36:02 +0530

    Use google books api to fetch book details

--- models/book.rb
+++ models/book.rb
@@ -10,15 +10,31 @@ class Book
   has n, :reservation
 
   def self.create_from_openlibrary(isbn)
+    params = details_from_google(isbn) || details_from_openlibrary(isbn)
+    Book.new(params).tap do |b|
+      b.book_copies << BookCopy.new
+    end if params
+  end
+
+  def self.details_from_openlibrary(isbn)
     details = ::Openlibrary::Data.find_by_isbn(isbn)
-    return unless details
-    params = {}.tap do |p|
+    {}.tap do |p|
       p[:isbn]   = details.identifiers["isbn_13"] ? details.identifiers["isbn_13"][0] : isbn
       p[:title]  = details.title
       p[:author] = details.authors.map{|a| a["name"]}.join(", ") if details.authors
       p[:photo_remote_url] = details.cover["medium"] if details.cover
-      p[:book_copies] = [BookCopy.new]
+    end if details
+  end
+
+  def self.details_from_google(isbn)
+    response = "https://www.googleapis.com/books/v1/volumes?q=isbn:#{isbn}&fields=items(volumeInfo(authors,description,imageLinks,industryIdentifiers,title))".to_uri.get.deserialise["items"][0]
+    return unless response
+    response = response["volumeInfo"]
+    {}.tap do |p|
+      p[:isbn] = isbn
+      p[:title] = response["title"]
+      p[:author] = response["authors"].join(", ")
+      p[:photo_remote_url] = response["imageLinks"]["thumbnail"] if response["imageLinks"]
     end
-    Book.create! params
   end
 end
