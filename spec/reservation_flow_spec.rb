02bdc734cc9d1614db3692259066c03703e4d639
Senthil V S <vss123@gmail.com>
2013-07-21 16:32:57 +0530

    Added spec for return

--- spec/reservation_flow_spec.rb
+++ spec/reservation_flow_spec.rb
@@ -1,7 +1,8 @@
 require File.dirname(__FILE__) + '/spec_helper.rb'
 
-feature "Reservation" do
+describe "Open library" do
   before do
+    [Book, BookCopy, User, Reservation].each {|model| model.all.destroy!}
     @book = Book.create(isbn: "9788131722428",
                 title: "The Pragmatic Programmer",
                 author: "Hunt",
@@ -10,13 +11,8 @@ feature "Reservation" do
     @user = User.create(employee_id: 19891, first_name: "Katta", last_name: "Durai")
   end
 
-  scenario "successfull reservation" do
-    visit '/'
-    expect(page).to have_content 'Tell me who you are'
-    fill_in("Employee ID", with: "#{@user.employee_id}\n")
-    expect(page).to have_content "Welcome #{@user.first_name}"
-    expect(page).to have_content "Pick & Scan"
-    fill_in("ISBN", with: "#{@book.isbn}\n")
+  it "should do successfull reservation" do
+    scan_user_id_and_book
     expect(page).to have_content "Thank you!"
     expect(page).to have_content "Thats it #{@user.first_name} #{@user.last_name}! You can take the book along with you, please return the book as soon as you are done with it"
     reservation = Reservation.last
@@ -25,11 +21,23 @@ feature "Reservation" do
     expect(reservation.user_id).to eq(@user.id)
   end
 
-  after do
-    [@book, @book_copy, @user].each{|m| m.destroy!}
+  it "should do successfull return" do
+    Reservation.create(user_id: @user.id, book_id: @book_copy.id, state: 'issued')
+    scan_user_id_and_book
+    expect(page).to have_content "Thank you!"
+    expect(page).to have_content "Thats it #{@user.first_name} #{@user.last_name}! You can now put the book where it belongs"
+    reservation = Reservation.last
+    expect(reservation.state).to eq('returned')
+    expect(reservation.book_id).to eq(@book_copy.id)
+    expect(reservation.user_id).to eq(@user.id)
   end
 end
 
-feature "Return" do
-
+def scan_user_id_and_book
+    visit '/'
+    expect(page).to have_content 'Tell me who you are'
+    fill_in("Employee ID", with: "#{@user.employee_id}\n")
+    expect(page).to have_content "Welcome #{@user.first_name}"
+    expect(page).to have_content "Pick & Scan"
+    fill_in("ISBN", with: "#{@book.isbn}\n")
 end
