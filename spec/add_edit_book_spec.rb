fb403d41542d3c595a17a2cce28477073c839c3f
Senthil V S <vss123@gmail.com>
2013-07-23 09:38:33 +0530

    Test covered edit book.

--- spec/add_edit_book_spec.rb
+++ spec/add_edit_book_spec.rb
@@ -6,10 +6,10 @@ describe "Open library" do
     @isbn = "9788131722428"
     @title = "The Pragmatic Programmer"
     @author = "Hunt"
+    @photo_remote_url = "http://bks4.books.google.co.in/books?id=kvZEJ9-Hqb0C&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api"
   end
 
   it "should add book" do
-    photo_remote_url = "http://bks4.books.google.co.in/books?id=kvZEJ9-Hqb0C&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api"
     google_api_response = {
       "items" => [{
       "volumeInfo" => {
@@ -25,7 +25,7 @@ describe "Open library" do
     }],
       "imageLinks" => {
       "smallThumbnail" => "http://bks4.books.google.co.in/books?id=kvZEJ9-Hqb0C&printsec=frontcover&img=1&zoom=5&edge=curl&source=gbs_api",
-      "thumbnail" => photo_remote_url}}}]
+      "thumbnail" => @photo_remote_url}}}]
     }
     google_api_url = "https://www.googleapis.com/books/v1/volumes?q=isbn:#{@isbn}&fields=items(volumeInfo(authors,description,imageLinks,industryIdentifiers,title))"
     stub_request(:get, google_api_url).to_return({:body => google_api_response.to_json, headers: {"content-type" => "application/json; charset=UTF-8"}})
@@ -41,24 +41,37 @@ describe "Open library" do
     expect(book_added.isbn).to eq(@isbn)
     expect(book_added.title).to eq(@title)
     expect(book_added.author).to eq(@author)
-    expect(book_added.photo_remote_url).to eq("http://bks4.books.google.co.in/books?id=kvZEJ9-Hqb0C&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api")
+    expect(book_added.photo_remote_url).to eq(@photo_remote_url)
     expect(book_added.book_copies.size).to eq(1)
   end
 
-  it "should add book copy" do
-    @book = Book.create(isbn: @isbn,
-                        title: "The Pragmatic Programmer",
-                        author: "Hunt",
-                        photo_remote_url: "http://bks4.books.google.co.in/books?id=kvZEJ9-Hqb0C&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api")
-    BookCopy.create(book: @book)
+  context "with existing book" do
+    before do
+      @book = Book.create(isbn: @isbn, title: @title, author: @author, photo_remote_url: @photo_remote_url)
+      BookCopy.create(book: @book)
+      visit "/donate"
+      fill_in "ISBN", with: "#{@book.isbn}\n"
+      expect(page).to have_content @book.title
+      expect(page).to have_content @book.author
+      expect(page).to have_content "This book has 1 copies."
+    end
 
-    visit "/donate"
-    fill_in "ISBN", with: "#{@isbn}\n"
-    expect(page).to have_content @title
-    expect(page).to have_content @author
-    expect(page).to have_content "This book has 1 copies."
-    fill_in "copies_to_add", with: 3
-    click_button "Add"
-    expect(page).to have_content "This book has 4 copies."
+    it "should add book copy" do
+      fill_in "copies_to_add", with: 3
+      click_button "Add"
+      expect(page).to have_content "This book has 4 copies."
+    end
+
+    it "should edit book" do
+      click_link "Edit"
+      fill_in "Title", with: "Modified Programmer"
+      fill_in "Author", with: "Killer"
+      click_button "Update"
+      expect(page).to have_content "Modified Programmer"
+      expect(page).to have_content "Killer"
+      @book.reload
+      expect(@book.title).to eq("Modified Programmer")
+      expect(@book.author).to eq("Killer")
+    end
   end
 end
